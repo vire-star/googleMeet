b8cfc67e3025a2e894f349f2e1abf0a2b91272f7
Virendra pratap <virendrapratap8744@gmail.com>
2026-03-06 17:15:53 +0530

    updated

--- src/middleware.js
+++ src/middleware.js
@@ -2,21 +2,15 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 export async function middleware(req) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
-  const token = await getToken({
-    req,
-    secret: process.env.NEXTAUTH_SECRET,
-  });
-
-  const { pathname } = req.nextUrl;
-
-  // user logged in but trying to access login page
-  if (pathname === "/user-auth" && token) {
+  // logged in user tries to go to login page
+  if (req.nextUrl.pathname === "/user-auth" && token) {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
-  // user not logged in and trying to access protected routes
-  if (!token && pathname !== "/user-auth") {
+  // user not logged in
+  if (!token && req.nextUrl.pathname !== "/user-auth") {
     return NextResponse.redirect(new URL("/user-auth", req.url));
   }
 
@@ -24,5 +18,5 @@ export async function middleware(req) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/:path*"],
 };
